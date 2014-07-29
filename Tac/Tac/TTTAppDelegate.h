a86c7c740973c8e17b2b8e0393ac2cba395d2e08
JaimeConnor <JaimeConnor@Jaimes-MacBook-Pro.local>
2014-07-29 09:35:39 -0400

    Create App

---
+++ Tac/Tac/TTTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TTTAppDelegate.h
+//  Tac
+//
+//  Created by Jaime Connor on 7/29/14.
+//  Copyright (c) 2014 Jaime Connor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
